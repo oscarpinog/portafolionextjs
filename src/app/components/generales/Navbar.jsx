29e0746054380a93d9e90a06108f19374a81e7ab
orodripi91008 <ogrodriguez@asesoftware.com>
2025-03-10 16:44:51 -0500

    Se ajusta navbar

--- src/app/components/generales/Navbar.jsx
+++ src/app/components/generales/Navbar.jsx
@@ -5,9 +5,19 @@ export default function Navbar({ collapsed, setCollapsed }) {
   return (
     <nav className="bg-gray-500 text-gray-200 shadow-md w-full">
       <div className="py-6 flex justify-between items-center w-full">
-        <h1 className="text-2xl font-extrabold tracking-wide text-gray-100 ml-6">
-          Portafolio
-        </h1>
+        <div className="flex flex-row">
+          <button
+            onClick={() => setCollapsed(!collapsed)}
+            className="ml-4 w-[80px] h-[40px] py-2 bg-gray-800 duration-300 ease-in-out text-white rounded flex items-center justify-center hover:bg-gray-700"
+          >
+            <AlignCenter className="w-5 h-5" />
+            <span className={`${collapsed ? "block" : "hidden"}`}>Menu</span>
+          </button>
+          <h1 className="text-2xl font-extrabold tracking-wide text-gray-100 ml-6">
+            Portafolio
+          </h1>
+        </div>
+
 
         <ul className="hidden md:flex space-x-6 mr-3">
           <li>
@@ -25,13 +35,6 @@ export default function Navbar({ collapsed, setCollapsed }) {
               Contacto
             </a>
           </li>
-          <button
-            onClick={() => setCollapsed(!collapsed)}
-            className="w-[80px] h-[40px] px-4 py-2 bg-gray-800 duration-300 ease-in-out text-white rounded flex items-center justify-center gap-2 hover:bg-gray-700"
-          >
-            <AlignCenter className="w-5 h-5" />
-            <span className={`${collapsed ? "block" : "hidden"}`}>Menu</span>
-          </button>
 
         </ul>
       </div>
